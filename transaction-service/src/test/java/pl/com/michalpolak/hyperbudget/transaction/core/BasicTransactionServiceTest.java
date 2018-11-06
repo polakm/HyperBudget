e44e53877485fbda536da41affbf005095433957
michal <polak_m@o2.pl>
2018-11-06 17:28:25 +0100

    Add test for transaction service

---
+++ transaction-service/src/test/java/pl/com/michalpolak/hyperbudget/transaction/core/BasicTransactionServiceTest.java
@@ -0,0 +1,53 @@
+package pl.com.michalpolak.hyperbudget.transaction.core;
+
+import org.junit.Test;
+import pl.com.michalpolak.hyperbudget.transaction.core.api.Transaction;
+import pl.com.michalpolak.hyperbudget.transaction.core.api.TransactionService;
+import pl.com.michalpolak.hyperbudget.transaction.core.spi.TransactionRepository;
+import pl.com.michalpolak.hyperbudget.transaction.data.InMemoryTransactionRepository;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BasicTransactionServiceTest {
+
+    @Test
+    public void addTransaction() {
+
+        //given
+        TransactionRepository repository = new InMemoryTransactionRepository();
+        TransactionService transactionService = new BasicTransactionService(repository);
+
+        Transaction transaction = new Transaction();
+        transaction.setTitle("Test");
+
+        //when
+       Transaction resultTransaction = transactionService.addTransaction(transaction);
+
+        //then
+        assertNotNull(resultTransaction);
+
+    }
+
+    @Test
+    public void allTrascations() {
+
+        //given
+        TransactionRepository repository = new InMemoryTransactionRepository();
+        TransactionService transactionService = new BasicTransactionService(repository);
+
+        Transaction transaction = new Transaction();
+        transaction.setTitle("Test");
+
+
+        //when
+        transactionService.addTransaction(transaction);
+        Set<Transaction> transactions =transactionService.allTrascations();
+        Transaction resultTransaction = transactions.iterator().next();
+
+        //then
+        assertNotNull(resultTransaction);
+
+    }
+}
