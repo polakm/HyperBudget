977fbe2b678eda3e9d10be9b8b312c1e36ca1e23
michal <polak_m@o2.pl>
2018-11-20 19:44:37 +0100

    Set transaction-service as service discovery client

--- transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/core/TransactionServiceApplication.java
+++ transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/core/TransactionServiceApplication.java
@@ -3,6 +3,7 @@ package pl.com.michalpolak.hyperbudget.transaction.core;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ import java.util.List;
 @Configuration
 @ComponentScan("pl.com.michalpolak")
 @EnableWebMvc
+@EnableDiscoveryClient
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
