aa28cc71c661a3fb39c008561b80fc7e1f593517
michal <polak_m@o2.pl>
2018-12-04 18:03:20 +0100

    Add gateway module

---
+++ gateway/src/main/java/pl/com/michalpolak/hyperbudget/GatewayApplication.java
@@ -0,0 +1,16 @@
+package pl.com.michalpolak.hyperbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ gateway/start.bat
@@ -0,0 +1,1 @@
+java -jar %CD%/build/libs/gateway-0.0.1-SNAPSHOT.jar
