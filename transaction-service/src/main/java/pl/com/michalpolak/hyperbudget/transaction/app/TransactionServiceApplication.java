99088b7f42a9e27888a2e6335caf408737e48810
michal <polak_m@o2.pl>
2018-12-09 01:54:47 +0100

    Integration transaction-service with zipkin

--- transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/app/TransactionServiceApplication.java
+++ transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/app/TransactionServiceApplication.java
@@ -3,11 +3,13 @@ package pl.com.michalpolak.hyperbudget.transaction.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableDiscoveryClient
+@ComponentScan("pl.com.michalpolak.hyperbudget")
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
