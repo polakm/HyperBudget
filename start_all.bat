4e199ac54bbbd5f20d315e1c809933f8a1abaa6d
michal <polak_m@o2.pl>
2018-12-09 01:52:41 +0100

    Add script for start all services

---
+++ start_all.bat
@@ -0,0 +1,26 @@
+cd %CD%/ui
+start start.bat
+cd ..
+
+cd %CD%/api-gateway
+start start.bat
+cd ..
+
+cd %CD%/service-discovery
+start start.bat
+cd ..
+
+cd %CD%/transaction-service
+start start.bat
+cd ..
+
+cd %CD%/transaction-aggregation-service
+start start.bat
+cd ..
+
+cd %CD%/tracing-service
+start start.bat
+cd ..
+
+
+
