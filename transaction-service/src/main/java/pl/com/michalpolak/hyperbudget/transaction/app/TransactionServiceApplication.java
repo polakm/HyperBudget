f7558f833d7a4b24a26990a1f8862dcb1bca555b
michal <polak_m@o2.pl>
2018-12-10 23:57:53 +0100

    Add api docs - swagger2 to transaction-service

--- transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/app/TransactionServiceApplication.java
+++ transaction-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/app/TransactionServiceApplication.java
@@ -3,11 +3,16 @@ package pl.com.michalpolak.hyperbudget.transaction.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableWebMvc
+@EnableSwagger2
 @EnableDiscoveryClient
 @ComponentScan("pl.com.michalpolak.hyperbudget")
 public class TransactionServiceApplication {
@@ -16,5 +21,14 @@ public class TransactionServiceApplication {
         SpringApplication.run(TransactionServiceApplication.class, args);
     }
 
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
