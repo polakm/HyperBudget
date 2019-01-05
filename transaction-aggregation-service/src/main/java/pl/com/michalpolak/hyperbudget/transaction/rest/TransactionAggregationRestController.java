eefce0124fb5cef96c6a40653b3efa6bef9f6060
michal <polak_m@o2.pl>
2019-01-05 20:59:11 +0100

    Add links to transaction summary rest api

--- transaction-aggregation-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/rest/TransactionAggregationRestController.java
+++ transaction-aggregation-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/rest/TransactionAggregationRestController.java
@@ -5,6 +5,8 @@ import org.joda.time.YearMonth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +15,11 @@ import pl.com.michalpolak.hyperbudget.transaction.core.TransactionSummary;
 import pl.com.michalpolak.hyperbudget.transaction.core.api.TransactionAggregationService;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -30,25 +36,54 @@ public class TransactionAggregationRestController {
 
 
     @RequestMapping(path="/summary", method = RequestMethod.GET)
-    public TransactionSummaryData transactionsSummary(){
+    public Resource<TransactionSummaryData> transactionsSummary(){
 
         List<AggregatedTransactionData> result = new ArrayList<>();
         TransactionSummary summary = service.getTransactionsSummary();
-
-        return  new TransactionSummaryData(summary);
+        TransactionSummaryData summaryData = new TransactionSummaryData(summary);
+        Link link = linkTo(methodOn(TransactionAggregationRestController.class).transactionsSummary()).withSelfRel();
+        Resource<TransactionSummaryData> resource = new Resource<>(summaryData, link);
+        return resource;
     }
 
 
 
     @RequestMapping(path="/summary/{year}/{month}", method = RequestMethod.GET)
-    public TransactionSummaryData transactionsSummaryPerMonth(@PathVariable("year") int year ,@PathVariable("month") int month){
+    public Resource<TransactionSummaryData> transactionsSummaryPerMonth(@PathVariable("year") int year , @PathVariable("month") int month){
 
         List<AggregatedTransactionData> result = new ArrayList<>();
-
         TransactionSummary summary = service.getTransactionsSummaryPeMonth(new YearMonth(year,month));
-        return  new TransactionSummaryData(summary);
+        TransactionSummaryData summaryData = new TransactionSummaryData(summary);
+        Iterable<Link> links = this.buildLinks(year, month);
+        Resource<TransactionSummaryData> resource = new Resource<>(summaryData,links);
+        return resource;
     }
 
+    private Collection<Link> buildLinks(int year, int month){
+
+        Collection<Link> links = new ArrayList<>();
+       Link selfLink = getSelfLink(year, month);
+       links.add(selfLink);
+       Link nextLink = getNextLink(year, month);
+       links.add(nextLink);
+       Link previousLink = getPreviousLink(year, month);
+       links.add(previousLink);
+       return links;
+    }
+
+    private Link getSelfLink( int year, int month) {
+        return linkTo(methodOn(TransactionAggregationRestController.class).transactionsSummaryPerMonth(year,month)).withSelfRel();
+    }
+
+    private Link getNextLink(int year, int month) {
+        YearMonth nextMonth = new YearMonth(year,month).plusMonths(1);
+        return linkTo(methodOn(TransactionAggregationRestController.class).transactionsSummaryPerMonth(nextMonth.getYear(),nextMonth.getMonthOfYear())).withRel("nextMonth");
+    }
+
+    private Link getPreviousLink(int year, int month) {
+        YearMonth previousMonth = new YearMonth(year,month).minusMonths(1);
+        return linkTo(methodOn(TransactionAggregationRestController.class).transactionsSummaryPerMonth(previousMonth.getYear(),previousMonth.getMonthOfYear())).withRel("previousMonth");
+    }
 
 
 }
