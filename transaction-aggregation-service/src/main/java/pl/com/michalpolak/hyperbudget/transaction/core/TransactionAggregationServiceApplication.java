cff0539749c1de559b11c28116ddb7c2cbdf0ad8
michal <polak_m@o2.pl>
2019-01-05 18:50:58 +0100

    Add swagger to transaction-aggregation-service

--- transaction-aggregation-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/core/TransactionAggregationServiceApplication.java
+++ transaction-aggregation-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/core/TransactionAggregationServiceApplication.java
@@ -8,16 +8,20 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import pl.com.michalpolak.hyperbudget.transaction.core.api.TransactionAggregationService;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.AccountService;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.CategoryService;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.TransactionService;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @Configuration
 @ComponentScan("pl.com.michalpolak")
-@EnableWebMvc
 @EnableDiscoveryClient
 @EnableFeignClients("pl.com.michalpolak")
 public class TransactionAggregationServiceApplication {
@@ -25,6 +29,15 @@ public class TransactionAggregationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionAggregationServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 
