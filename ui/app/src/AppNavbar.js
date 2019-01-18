8983fa6894140636d4b21797e1bcb3ace8625de1
michal <polak_m@o2.pl>
2019-01-18 18:51:09 +0100

    Show application version on navbar #wip

--- ui/app/src/AppNavbar.js
+++ ui/app/src/AppNavbar.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavItem, NavLink, Badge } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default class AppNavbar extends Component {
   constructor(props) {
     super(props);
-    this.state = {isOpen: false};
+
+    // FIXME: load application name and version from config file
+    var application = {
+        name: 'HyperBudget',
+        version: '0.6.0-SNAPSHOT'
+    };
+    this.state = {application: application, isOpen: false};
     this.toggle = this.toggle.bind(this);
   }
 
@@ -17,8 +23,14 @@ export default class AppNavbar extends Component {
 
   render() {
     return <Navbar color="dark" dark expand="md">
-      <NavbarBrand tag={Link} to="/">HyperBudget</NavbarBrand>
-      <NavbarToggler onClick={this.toggle}/>
+     <Nav>
+      <NavbarBrand tag={Link} to="/">{this.state.application.name}</NavbarBrand>
+       </Nav>
+       <Nav className="ml-auto" navbar>
+        <NavItem>
+            <h5> <Badge pill color="light">{this.state.application.version}</Badge></h5>
+        </NavItem>
+       </Nav>
     </Navbar>;
   }
 }
