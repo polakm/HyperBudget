9ce9725af4eb6be6bf67ff54a449bca23f006dc2
michal <polak_m@o2.pl>
2019-01-28 19:05:48 +0100

    Use callbacks for asynchronous getting categories and accounts from rest services #performance

--- transaction-summary-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/data/RestClientsTransactionRepository.java
+++ transaction-summary-service/src/main/java/pl/com/michalpolak/hyperbudget/transaction/data/RestClientsTransactionRepository.java
@@ -1,5 +1,8 @@
 package pl.com.michalpolak.hyperbudget.transaction.data;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import org.joda.time.YearMonth;
 import pl.com.michalpolak.hyperbudget.transaction.core.api.TransactionInfo;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.Account;
@@ -12,6 +15,8 @@ import pl.com.michalpolak.hyperbudget.transaction.data.spi.TransactionService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 class RestClientsTransactionRepository implements TransactionRepository {
 
@@ -27,15 +32,43 @@ class RestClientsTransactionRepository implements TransactionRepository {
     }
     @Override
     public List<TransactionInfo> getTransactionInfosByMonth(YearMonth month) {
-        List<TransactionInfo> transactionInfos = new ArrayList<TransactionInfo>();
 
+        List<TransactionInfo> transactionInfos = new ArrayList<TransactionInfo>();
+        ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
         List<Transaction> transactions = transactionService.transactionList();
         transactions.stream().filter(t -> t.getExecutionDate().getMonthOfYear() == month.getMonthOfYear() && t.getExecutionDate().getYear() == month.getYear()).forEach(t -> {
 
-            Category category = categoryService.getCategory(t.getCategoryId());
-            Account account = accountService.getAccount(t.getAccountId());
-            transactionInfos.add(new TransactionInfo(t, category, account));
+            ListenableFuture<Category> categoryFuture = service.submit(() -> categoryService.getCategory(t.getCategoryId()));
+            ListenableFuture<Account> accountFuture = service.submit(() -> accountService.getAccount(t.getAccountId()));
+            TransactionInfo transactionInfo = collectData(t, categoryFuture, accountFuture);
+            transactionInfos.add(transactionInfo);
         });
         return transactionInfos;
+    }
+
+    private TransactionInfo collectData(Transaction t, ListenableFuture<Category> categoryFuture, ListenableFuture<Account> accountFuture) {
+
+        Category category = readCategoryFromFuture(categoryFuture);
+        Account account = readAccountFromFuture(accountFuture);
+        return new TransactionInfo(t, category, account);
+    }
+
+    private Account readAccountFromFuture(ListenableFuture<Account> accountFuture) {
+
+        try {
+            return accountFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private Category readCategoryFromFuture(ListenableFuture<Category> categoryFuture) {
+
+        try {
+            return categoryFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
