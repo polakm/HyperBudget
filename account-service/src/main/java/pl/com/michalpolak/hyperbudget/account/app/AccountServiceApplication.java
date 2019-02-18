f90063c6062f1f141d7d7691b4052c6367d0e2fa
michal <polak_m@o2.pl>
2019-02-18 18:31:27 +0100

    Extract swagger configuration to new class #refactor

--- account-service/src/main/java/pl/com/michalpolak/hyperbudget/account/app/AccountServiceApplication.java
+++ account-service/src/main/java/pl/com/michalpolak/hyperbudget/account/app/AccountServiceApplication.java
@@ -3,12 +3,7 @@ package pl.com.michalpolak.hyperbudget.account.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -19,16 +14,6 @@ public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
-    }
-
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
     }
 
 }
