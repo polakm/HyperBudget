0a13e6688d50bc6cc0682500f00676b6c1159a51
michal <polak_m@o2.pl>
2019-02-25 18:43:55 +0100

    Repackage GatewayApplication #refactor

--- api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package pl.com.michalpolak.hyperbudget;
+package pl.com.michalpolak.hyperbudget.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
