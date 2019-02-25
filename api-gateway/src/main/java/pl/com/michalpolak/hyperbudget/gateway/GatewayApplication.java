44ec775369d2ac2eb3c528be02aabd3ed3ae41cb
michal <polak_m@o2.pl>
2019-02-25 18:46:58 +0100

    Remove commented code #refactor

--- api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
@@ -16,12 +16,5 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
- /*   @Bean
-    public PatternServiceRouteMapper serviceRouteMapper() {
-        return new PatternServiceRouteMapper(
-                "(?<name>^.+)-(?<version>v.+$)",
-                "${version}/${name}");
-    }*/
-
+    
 }
