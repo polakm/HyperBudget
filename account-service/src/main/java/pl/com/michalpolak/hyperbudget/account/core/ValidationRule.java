6938c138c66eb5e4d29e86a77ba94f39616bd6fc
michal <polak_m@o2.pl>
2019-02-25 18:52:30 +0100

    Change access to package in class form account-service package core #refactor

--- account-service/src/main/java/pl/com/michalpolak/hyperbudget/account/core/ValidationRule.java
+++ account-service/src/main/java/pl/com/michalpolak/hyperbudget/account/core/ValidationRule.java
@@ -3,6 +3,6 @@ package pl.com.michalpolak.hyperbudget.account.core;
 import pl.com.michalpolak.hyperbudget.account.core.api.Account;
 import pl.com.michalpolak.hyperbudget.account.core.api.InvalidAccountException;
 
-public interface ValidationRule {
+interface ValidationRule {
     void validate(Account account) throws InvalidAccountException;
 }
--- api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/pl/com/michalpolak/hyperbudget/gateway/GatewayApplication.java
@@ -16,5 +16,5 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-    
+
 }
