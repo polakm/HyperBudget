a0c530a111b6917b8ebada319a53de083b3efc30
michal <polak_m@o2.pl>
2019-04-10 22:26:37 +0200

    Change event publisher test to unit test

--- transaction-service/src/test/java/pl/com/michalpolak/hyperbudget/transaction/event/EventPublisherTest.java
+++ transaction-service/src/test/java/pl/com/michalpolak/hyperbudget/transaction/event/EventPublisherTest.java
@@ -1,34 +1,54 @@
 package pl.com.michalpolak.hyperbudget.transaction.event;
 
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.joda.money.Money;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import pl.com.michalpolak.hyperbudget.transaction.core.api.Transaction;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.EventPublisher;
 import pl.com.michalpolak.hyperbudget.transaction.core.spi.TransactionEvent;
-import pl.com.michalpolak.hyperbudget.transaction.test.IntegrationTest;
 
 import java.util.UUID;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static pl.com.michalpolak.hyperbudget.transaction.event.EventConfiguration.createEventPublisher;
 
-@IntegrationTest // need working kafka
 public class EventPublisherTest {
 
 
     @Test
     public void publish() {
-        EventPublisher publisher = createEventPublisher("test","http://message-broker:9092","test-client");
-        for(int i=0; i<=100; i++) {
 
-            Transaction transaction = new Transaction();
-            transaction.setAccountId(UUID.randomUUID().toString());
-            transaction.setAmount(Money.parse("USD 1023.33"));
-            transaction.setExecutionDate(DateTime.now());
-            transaction.setTitle("TRANSACTION-TEST "+i);
-            transaction.setCategoryId(UUID.randomUUID().toString());
-            TransactionEvent event = new TransactionEvent(TransactionEvent.Actions.UPDATED, transaction);
-            publisher.publish(event);
-        }
+        //given
+        MockProducer<Long, String> producer = new MockProducer<>(true, new LongSerializer(), new StringSerializer());
+        EventPublisher publisher = createEventPublisher("test-topic",  mockProducerCreator(producer));
+
+        Transaction transaction = new Transaction();
+        transaction.setAccountId(UUID.randomUUID().toString());
+        transaction.setAmount(Money.parse("USD 1023.33"));
+        transaction.setExecutionDate(DateTime.now());
+        transaction.setTitle("TRANSACTION-TEST ");
+        transaction.setCategoryId(UUID.randomUUID().toString());
+        TransactionEvent event = new TransactionEvent(TransactionEvent.Actions.UPDATED, transaction);
+
+        //when
+        publisher.publish(event);
+
+        //then
+        producer.history().stream().findFirst().ifPresent(e -> {
+            assertEquals(e.topic(), "test-topic");
+            assertNotNull(e.value());
+        });
+    }
+
+    private ProducerCreator mockProducerCreator(MockProducer<Long, String> producer) {
+        ProducerCreator producerCreator = mock(ProducerCreator.class);
+        when(producerCreator.createProducer()).thenReturn(producer);
+        return producerCreator;
     }
 }
